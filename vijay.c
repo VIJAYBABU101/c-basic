99482c9876267d8e05324b5fc1f99f5a722ac9d3
Vijaybabu <92910981+VIJAYBABU101@users.noreply.github.com>
2021-11-05 23:32:40 -0700

    Add files via upload

--- vijay.c
+++ vijay.c
@@ -1,5 +1,19 @@
 #include<stdio.h>
 void main()
 {
-	printf("vijay thalapathy");
-} 
+	int a, b, s, t, u, v, w;
+	a = 10;
+	b = 2;
+	
+	s = a + b;
+	t = a - b;
+	u = a * b;
+	v = a / b;
+	w = a % b;
+	
+	printf("\nsum of %d and %d is %d", a, b, s);
+	printf("\nsubtraction of % d and %d is %d", a, b, t);
+	printf("\nmultipicaion of %d and %d is %d", a, b, u);
+	printf("\ndivision of %d and %d is %d", a, b, v);
+	printf("\nmodulo division of %d and %d is %d", a, b, w);
+}
