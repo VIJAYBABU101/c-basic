3f7cbbd6abee68b57bf7d57a583caef5d5035055
Vijaybabu <92910981+VIJAYBABU101@users.noreply.github.com>
2021-12-11 06:44:03 -0800

    Add files via upload

---
+++ the number positive or negative.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main()
+{
+	int number;
+	printf("enter the number");
+	scanf("%d",&number);
+	if(number>0)
+	{
+		printf("the number is positive");
+	}
+	else
+	printf("the number is negative");
+}
