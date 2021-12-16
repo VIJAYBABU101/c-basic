59bcea9cad934d6a1fd8c7b9614398a63fb675fb
Vijaybabu <92910981+VIJAYBABU101@users.noreply.github.com>
2021-12-16 21:33:35 +0530

    Add files via upload

---
+++ months.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+void main()
+{
+	int number;
+	printf("enter the number");
+	scanf("%d",&number);
+	if(number==1)
+{
+	printf("january");
+}
+else if(number==2)
+{
+	printf("february");
+}
+else if(number==3)
+{
+	printf("march");
+}
+else if(number==4)
+{
+	printf("april");
+}
+else if(number==5)
+{
+	printf("may");
+}
+else if(number==6)
+{
+	printf("june");
+}
+else if(number==7)
+{
+	printf("july");
+}
+else if(number==8)
+{
+	printf("august");
+}
+else if(number==9)
+{
+	printf("september");
+}
+else if(number==10)
+{
+	printf("october");
+}
+else if(number==11)
+{
+	printf("november");
+}
+else if(number==12)
+{
+	printf("december");
+}
+else
+{
+	printf("invalid input");
+}
+}
+
