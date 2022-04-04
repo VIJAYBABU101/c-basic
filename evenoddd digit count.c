9fee5290852a30534bcbf1c73a7c6e38f17fb4e3
Vijaybabu <92910981+VIJAYBABU101@users.noreply.github.com>
2022-04-03 22:45:06 -0700

    Add files via upload

---
+++ evenoddd digit count.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+	int n,r,edc=0,odc=0;
+	scanf("%d",&n);
+	while(n>0)
+	{
+		r=n%10;
+		if(r%2==0) edc++;
+		else odc++;
+		n=n/10;
+	}
+	printf("even digits:%d\n",edc);
+	printf("odd digits:%d\n",odc);
+}
