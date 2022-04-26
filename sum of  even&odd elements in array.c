784fdf1f51ae04029d9f7b82bd42a0b613be4f43
Vijaybabu <92910981+VIJAYBABU101@users.noreply.github.com>
2022-04-25 22:53:57 -0700

    Add files via upload

---
+++ sum of  even&odd elements in array.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int n;
+	scanf("%d",&n);
+	int arr[n];
+	int i;
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+	int es=0,os=0;
+	for(i=0;i<n;i++)
+	{
+		if(arr[i]%2==0)
+		{
+			es=es+arr[i];
+		}
+		else
+		{
+			os=os+arr[i];
+		}
+	}
+	printf("%d %d",es,os);
+}
